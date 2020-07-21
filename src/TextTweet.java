65616a73a623201a38df28ffe1b763a19d220a39
gentoopng <58542728+gentoopng@users.noreply.github.com>
2020-07-22 02:27:55 +0900

    Add imports

--- src/TextTweet.java
+++ src/TextTweet.java
@@ -1,2 +1,8 @@
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.Status;
+import twitter4j.StatusUpdate;
+
 public class TextTweet {
 }
