567225b087101ce4f2b0dbd71db0aa6249606ba9
gentoopng <58542728+gentoopng@users.noreply.github.com>
2020-07-23 19:37:53 +0900

    Add imports

--- src/SimpleTwitterGUI.java
+++ src/SimpleTwitterGUI.java
@@ -1,2 +1,8 @@
-public class SimpleTwitterGUI {
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+
+public class SimpleTwitterGUI extends JFrame {
 }
