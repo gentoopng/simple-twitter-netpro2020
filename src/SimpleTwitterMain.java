cbe6d1836af07aca67cbed72d3cc1de1eb757308
gentoopng <58542728+gentoopng@users.noreply.github.com>
2020-07-23 16:43:36 +0900

    Tweet mode

--- src/SimpleTwitterMain.java
+++ src/SimpleTwitterMain.java
@@ -1,14 +1,66 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SimpleTwitterMain {
-    public static void main(String[] args) {
-        try {
-            ServerSocket server = new ServerSocket(5001);
-            System.out.println("Server is waiting at port 5001");
+    public static void main(String[] args) throws IOException {
+        TextTweet textTweet;
+        Integer mode;
 
+        ServerSocket server = null;
+        Socket socket = null;
+        ObjectInputStream ois = null;
+        ObjectOutputStream oos = null;
+
+        try {
+            server = new ServerSocket(5001);
+            socket = server.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (ois == null) {
+                ois = new ObjectInputStream(socket.getInputStream());
+            }
+
+            int counter = 100;
+
+            //String tweetString = "";
+            //String timelineString = "";
+
+            while (counter-- > 0) {
+                try {
+                    textTweet = (TextTweet) ois.readObject();
+                    mode = textTweet.getMode();
+
+                    switch (mode) {
+                        case 0: //check TL mode
+                            break;
+                        case 1: //tweet mode
+                            textTweet.run();
+                            break;
+                        default:
+                            System.out.println("error, try again");
+                            break;
+                    }
+
+                    if (oos == null) {
+                        oos = new ObjectOutputStream(socket.getOutputStream());
+                    }
+
+                    if (oos != null) {
+                        oos.writeObject(textTweet);
+                        oos.flush();
+                    }
+
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
