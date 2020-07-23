5a48490d932febb924618c3dbd7fda3325ed4ba6
gentoopng <58542728+gentoopng@users.noreply.github.com>
2020-07-23 18:07:44 +0900

    GetTimeLine

--- src/GetTimeline.java
+++ src/GetTimeline.java
@@ -1,2 +1,29 @@
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import java.util.List;
+
 public class GetTimeline {
+
+    public String get(int count) {
+        StringBuffer result = new StringBuffer();
+        Twitter twitter = new TwitterFactory().getInstance();
+
+        try {
+            Paging page = new Paging();
+            page.setCount(count);
+            List<Status> statusList = twitter.getHomeTimeline(page);
+            for (Status status: statusList) {
+                result.append(status.getUser().getName() + " @" + status.getUser().getScreenName() + "\n");
+                result.append(status.getText() + "\n");
+            }
+        } catch (TwitterException e) {
+            e.printStackTrace();
+            result.append(e.getErrorMessage());
+        }
+
+        return result.toString();
+    }
 }
